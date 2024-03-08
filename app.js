41f37343b5dbac6a8ca6984d575dba2c1c2974f3
sheikhabdullahahmed <sheikhabdullahahmed>
2024-03-09 01:25:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// let a =20;
+// let b =30;
+// console.log ("30 <= 30", a <= b);
+
+// logicla Opearator &&  
+
+// let a=40;
+// let b =30;
+
+// let con1 = a > b ;
+// let con2 = a === 30; 
+// console.log ("con1 && con2 =" , con1 && con2);
+
+// let a = 40; 
+// let b =50; 
+
+// // let con1 = a > b ; 
+// if (
+//     a === b
+//     // console.log("true")
+// )
+// console.log(con1)
+
+
+// let a = 2000; 
+// let b = 300; 
+// let con1 = a > b ;
+// if ( a > b) {
+//     console.log ("true")
+// }  
+// else{
+//     alert("false")
+// }
+// console.log(con1)
+
+// let a = 10; 
+// let b = 10;
+// if (a === b) {
+//     alert("True")
+// }
+// else{
+//     alert("falsex")
+// }
+
+
+
+
+
+// let con1 =20;
+// let con2 =30;a
+// console.log ("con1 && con2" , "a < b , a===b" )
+
+// let a =20;
+// let b = 10;
+
+
+// let con1 = a > b;
+// let con2 = a===20;
+// console.log ('con1 && con2' , con1 && con2);
+
+// let b = 30;
+// let c = 20;
+// let con2 = b > c;
+// let con3 = b === 30;
+// console.log ("con2 && con3" , con2 && con3);
+
+
+// let a = 30;
+// let b = 20;
+// let con2 = a > b;
+// let con3 = a === 30;
+// console.log ("con2 || con3" , con2 || con3);  
+
+// || pipe signal 
+
+// let a = 10;
+// let b = 5; 
+// let con1 = a > b;
+// let con2 = a === 10;
+// console.log (" con1 && con2" , con1 && con2);  
